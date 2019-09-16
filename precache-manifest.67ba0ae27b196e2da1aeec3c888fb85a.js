d688c6ac530ce93ddd9d54f3888c00a7a5fc4721
Muhammad <newuser@Raza-Macbook-Pro.local>
2019-09-15 22:50:45 -0400

    Updates

---
+++ precache-manifest.67ba0ae27b196e2da1aeec3c888fb85a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54efc910dd76411dd5182dc898619e7",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "0d7cd2867a8f9db2a246",
+    "url": "/monsters/static/css/main.39585494.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "0d7cd2867a8f9db2a246",
+    "url": "/monsters/static/js/main.03e366e4.chunk.js"
+  },
+  {
+    "revision": "7af81f04658571a85612",
+    "url": "/monsters/static/js/runtime~main.35953b23.js"
+  }
+]);
